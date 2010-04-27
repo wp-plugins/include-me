84ded3fe7f335d30759f8fb44f092ab7187a5b6b
satollo <satollo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-27 20:10:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/include-me/trunk@234525 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: Include Me
+Plugin URI: http://www.satollo.net/plugins/include-me
+Description: Directly include HTML or PHP in any post/page to embed advanced functionalities.
+Version: 1.0.0
+Author: Satollo
+Author URI: http://www.satollo.net
+Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
+*/
+
+/*	Copyright 2008 Satollo  (email : satollo@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+$includeme_options = get_option('includeme');
+
+add_action('admin_menu', 'includeme_admin_head');
+function includeme_admin_head()
+{
+    add_options_page('Include Me', 'Include Me', 'manage_options', 'includeme-me/options.php');
+}
+*/
+
+add_shortcode('includeme', 'includeme_call');
+
+function includeme_call($attrs, $content=null) {
+    global $includeme_options;
+
+    $file = $attrs['file'];
+    if ($file) {
+        if ($file[0] != '/') $file = ABSPATH . $file;
+
+        ob_start();
+        include($file);
+        $buffer = ob_get_contents();
+        ob_end_clean();
+    }
+    else {
+        $tmp = '';
+        foreach ($attrs as $key=>$value) {
+            $value = str_replace('&amp;', '&', $value);
+            if ($key == 'src') $value = strip_tags($value);
+            $tmp .= ' ' . $key . '="' . $value . '"';
+        }
+        $buffer = '<iframe' . $tmp . '></iframe>';
+    }
+    return $buffer;
+}
+?>
